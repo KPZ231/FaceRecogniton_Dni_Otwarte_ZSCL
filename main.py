e43587e0597c0ae0c88b94cfb6e070c029eb775c
Kacper Duda <kapieksperimental@gmail.com>
2023-02-16 21:01:03 +0100

    updating main.py

--- main.py
+++ main.py
@@ -2,18 +2,69 @@ import cv2
 import numpy as np
 from tensorflow.python.keras.models import load_model
 
+
+#region Modele Danych
+
 model = load_model("data\ER_model_training12ep.h5")
 
 face_cascade = cv2.CascadeClassifier("data\haarcascade_frontalface_default.xml")
 
+#endregion 
+#region Wczytywanie Danych
 emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 emotion_colors = [(0, 0, 255), (0, 0, 255), (0, 0, 255), (0, 255, 0), (0, 0, 255), (255, 0, 0), (255, 255, 255)]
+#endregion
+
+#paramety
+frame_count = 0
+start_time = cv2.getTickCount()
+font = cv2.FONT_HERSHEY_SIMPLEX
+pos = (10, 30)
+font_scale = 0.5
+font_color = (0, 255, 0)
+line_type = 2
 
 
-cap = cv2.VideoCapture(0)
+#region camera index
+camera_index = 0
+available_cameras = []
+
+for i in range(10):
+    cap = cv2.VideoCapture(i)
+    if cap.isOpened():
+        available_cameras.append(i)
+    cap.release()
+
+
+if not available_cameras:
+    print("No cameras available. Exiting.")
+    exit()
+
+print(f"Available cameras: {available_cameras}")
+camera_input = int(input(" Camera To Select: "))
+
+if camera_index not in available_cameras:
+    camera_index = int(input(f"Select a camera from {available_cameras}: "))
+
+
+#endregion
+
+cap = cv2.VideoCapture(camera_input)
 
 while True:
     ret, frame = cap.read()
+
+    if ret:
+        frame_count += 1
+
+        elapsed_time = (cv2.getTickCount() - start_time) / cv2.getTickFrequency()
+        current_fps = int(frame_count / elapsed_time)
+
+        fps_text = f"FPS: {current_fps}"
+        cv2.putText(frame, fps_text, pos, font, font_scale, font_color, line_type)
+    else:
+        break
+
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -21,8 +72,6 @@ while True:
 
     for (x, y, w, h) in faces:
         roi_gray = gray[y:y+h, x:x+w]
-        
-
         roi_gray = cv2.resize(roi_gray, (48, 48))
         
         roi_gray = np.expand_dims(roi_gray, axis=-1)
