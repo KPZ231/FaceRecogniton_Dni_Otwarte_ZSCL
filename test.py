3cd3823d512687a3ce6e1d9b70c2301939184ae7
Kacper Duda <62105418+KPZ231@users.noreply.github.com>
2023-03-06 07:15:52 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,87 +1,134 @@
 import cv2
 import numpy as np
 from tensorflow.python.keras.models import load_model
-import threading
+import tensorflow as tf
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
-# Model and cascade files
-model_file = "data\ER_model_training12ep.h5"
-cascade_file = ["data\haarcascade_frontalface_default.xml", "data\haarcascade_profileface.xml"]
+width = int(input("Select Width Of Screen: "))
+height = int(input("Select Height Of Screen: "))
 
-for i in  range(len(cascade_file)):
-    face_cascade = cv2.CascadeClassifier(cascade_file[i])    
+#░█░█░█▀█░▀▀█
+#░█▀▄░█▀▀░▄▀░
+#░▀░▀░▀░░░▀▀▀`
+#region Modele Danych
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Konfiguracja dla dostępnej karty graficznej
+    tf.config.experimental.set_virtual_device_configuration(
+        gpus[0],
+        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024 * 4)]
+    )
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Błąd konfiguracji
+    print(e)
 
 
-# Load model and cascade classifier
-model = load_model(model_file)
+model = tf.keras.models.load_model("data\ER_model_training12ep.h5")
+
+face_cascade = cv2.CascadeClassifier("data\haarcascade_frontalface_default.xml")
 
 
-# Emotion labels and colors
+
+
+#endregion 
+#region Wczytywanie Danych
 emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 emotion_colors = [(0, 0, 255), (0, 0, 255), (0, 0, 255), (0, 255, 0), (0, 0, 255), (255, 0, 0), (255, 255, 255)]
+#endregion
 
-# Camera parameters
+#paramety
+frame_count = 0
+start_time = cv2.getTickCount()
+font = cv2.FONT_HERSHEY_SIMPLEX
+pos = (10, 30)
+font_scale = 0.5
+font_color = (0, 255, 0)
+line_type = 1
+
+
+#region camera index
 camera_index = 0
 available_cameras = []
+
 for i in range(10):
     cap = cv2.VideoCapture(i)
     if cap.isOpened():
         available_cameras.append(i)
     cap.release()
 
+
 if not available_cameras:
     print("No cameras available. Exiting.")
     exit()
 
 print(f"Available cameras: {available_cameras}")
-camera_input = int(input("Camera To Select: "))
+camera_input = int(input(" Camera To Select: "))
 
 if camera_index not in available_cameras:
     camera_index = int(input(f"Select a camera from {available_cameras}: "))
 
+
+#endregion
+
 cap = cv2.VideoCapture(camera_input)
 
-# Thread function to read frames from the camera
-def read_camera():
-    global cap, frame, ret
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
 
-# Thread function to process frames for emotion detection
-def detect_emotions():
-    global frame
-    while True:
-        if not ret:
-            break
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-        for (x, y, w, h) in faces:
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_gray = cv2.resize(roi_gray, (48, 48))
-            roi_gray = np.expand_dims(roi_gray, axis=-1)
-            roi_gray = np.expand_dims(roi_gray, axis=0)
-            roi_gray = roi_gray / 255.0
-            emotion_probabilities = model.predict(roi_gray)
-            emotion_index = np.argmax(emotion_probabilities[0])
-            emotion = emotion_labels[emotion_index]
-            cv2.rectangle(frame, (x, y), (x+w, y+h), emotion_colors[emotion_index], 2)
-            cv2.putText(frame, emotion, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, emotion_colors[emotion_index], 2)
-        cv2.imshow('Emotion Detection ZSCL By KPZ', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+while True:
+    ret, frame = cap.read()
 
-# Start threads
-frame = None
-ret = False
-camera_thread = threading.Thread(target=read_camera)
-emotion_thread = threading.Thread(target=detect_emotions)
-camera_thread.start()
-emotion_thread.start()
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
-# Wait for threads to finish
-camera_thread.join()
-emotion_thread.join()
+
+    cv2.namedWindow("Emotion Detection ZSCL By KPZ")
+    cv2.resizeWindow("Emotion Detection ZSCL By KPZ" , width, height)
+
+    if ret:
+        frame_count += 1
+
+        elapsed_time = (cv2.getTickCount() - start_time) / cv2.getTickFrequency()
+        current_fps = int(frame_count / elapsed_time)
+
+        fps_text = f"FPS: {current_fps}"
+        cv2.putText(frame, fps_text, pos, font, font_scale, font_color, line_type)
+    else:
+        break
+
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for (x, y, w, h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_gray = cv2.resize(roi_gray, (48, 48))
+        
+        roi_gray = np.expand_dims(roi_gray, axis=-1)
+        roi_gray = np.expand_dims(roi_gray, axis=0)
+
+        roi_gray = roi_gray / 255.0
+
+        emotion_probabilities = model.predict(roi_gray)
+        emotion_index = np.argmax(emotion_probabilities[0])
+        emotion = emotion_labels[emotion_index]
+      
+        cv2.rectangle(frame, (x, y), (x+w, y+h), emotion_colors[emotion_index], 1)
+        
+        cv2.putText(frame, emotion, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, emotion_colors[emotion_index], 1)
+        
+
+    cv2.imshow('Emotion Detection ZSCL By KPZ', frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
 
 cap.release()
-cv2.destroyAllWindows()
+
+cv2.destroyAllWindows
